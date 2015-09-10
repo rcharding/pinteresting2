6e9245bad7f918ee6b89c99832febfe049a6c570
Chris Harding <chris_84harding@hotmail.com>
2015-09-10 16:36:26 -0400

    testing changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,10 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
+
+  #######################
+  ######################
+  #####################
   # was:
   #config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.serve_static_files = true
